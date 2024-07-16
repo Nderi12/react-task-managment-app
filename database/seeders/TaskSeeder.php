4c5212392bbdaaea1286707004edbba96aab823d
Nderi12 <nderikamau1212@gmail.com>
2024-07-16 18:41:28 +0300

    Adding react components

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -14,21 +14,30 @@ class TaskSeeder extends Seeder
      */
     public function run(): void
     {
-        $projects = Project::all();
+        $project_ids = Project::all()->pluck('id')->toArray();
 
+        $now = now();
         $tasks = [];
-
-        foreach ($projects as $project) {
-            for ($i = 1; $i <= 10; $i++) {
-
-                Task::create([
-                    'project_id' => $project->id,
-                    'priority' => $project->id,
-                    'title' => "Task " . $project->name,
-                    'description' => "Description for Task " . $project->name,
-                ]);
-            }
+        $project_priorities = [];
+        foreach ($project_ids as $project_id) {
+            $project_priorities[$project_id] = 0;
         }
 
+        for ($i = 1; $i <= 10; $i++) {
+            $project_id = $project_ids[array_rand($project_ids)];
+            $project_priorities[$project_id]++;
+
+            $tasks[] = [
+                'project_id' => $project_id,
+                'priority' => $project_priorities[$project_id],
+                'title' => "Task " . $project_priorities[$project_id],
+                'description' => "Description for Task " . $project_priorities[$project_id],
+                
+                'created_at' => $now,
+                'updated_at' => $now,
+            ];
+        }
+
+        Task::insert($tasks);
     }
 }
